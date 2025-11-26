b5e5f3d16285a5257639dc15c8cbdcf2e8b5276f
LindaleRock <lindalerock@gmail.com>
2025-11-26 19:15:29 +0600

    dashboard page fixed

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,13 +3,6 @@ import { useSession } from 'next-auth/react';
 import React from 'react';
 
 const page = () => {
-  const { data: session, status } = useSession();
-  if (!session) {
-    console.log('session fail');
-    redirect('/login');
-  }
-
-  console.log(session);
   return <div>this is dashboard</div>;
 };
 
